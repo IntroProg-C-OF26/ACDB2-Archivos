0a305a01f4e6110fd0a61143d8dbed61b7a42040
Pedro Daniel Irene Robalino <pdirene@utpl.edu.ec>
2026-01-22 10:30:26 -0500

    act

--- EscrituraLectura_Archivos/src/Test1.java
+++ EscrituraLectura_Archivos/src/Test1.java
@@ -1,5 +1,19 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("hola");
+        Scanner tcl = new Scanner(System.in);
+        int n1, n2, respuesta;
+        System.out.print("Deme 2#: ");
+        try {
+            n1 = tcl.nextInt();
+            n2 = tcl.nextInt();
+            respuesta = n1 / n2;
+            System.out.println("Espuesta = " + respuesta);
+        } catch (InputMismatchException e) {
+            System.out.println("ERROR DE INGRESO DE DATOS");
+        } catch (ArithmeticException e) {
+            System.out.println("ERROR MATEMATICO DIVISION PARA CERO");
+        }
     }
 }
